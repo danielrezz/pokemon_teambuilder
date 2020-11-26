8fa59ae9a348508f97ddaa74dea7129a478870e4
Daniel Reza <daniel.reza1007@gmail.com>
2020-11-26 00:55:36 -0600

    updating AJAX calls and some CSS

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function searchPokemon() {
 
     var pokemonName = pokemonInput.value;
     var queryURL = "https://pokeapi.co/api/v2/pokemon/" + pokemonName.toLowerCase();
+    var pokedexURL = "https://pokeapi.co/api/v2/pokemon-species/" + pokemonName.toLowerCase();
 
         $.ajax({
             url: queryURL,
@@ -27,6 +28,33 @@ function searchPokemon() {
             var spriteLink = response.sprites.front_default;
 
             $("#pokemon-sprite-img").attr("src", spriteLink);
+
+            var pokemonType = response.types[0].type.name;
+            console.log(pokemonType);
+
+            $("#pokemon-type").text("Type: " + pokemonType.charAt(0).toUpperCase() + pokemonType.slice(1));
+
+            var ability1 = response.abilities[0].ability.name;
+            var ability2 = response.abilities[1].ability.name;
+            $("#pokemon-abilities").text("Abilities: " + ability1 + ", " + ability2);
+
+        });
+
+        $.ajax({
+            url: pokedexURL,
+            method: "GET"
+        }).then(function(response) {
+
+            console.log(response);
+
+            var pokemonSpecies = response.genera[7].genus;
+            console.log(pokemonSpecies);
+
+            $("#pokemon-fact").text(response.flavor_text_entries[1].flavor_text);
+
+            // $("#pokemon-type").css("background-color", response.color.name);
+
+            $("#pokemon-species").text("Species: " + pokemonSpecies);
 
         });
     };
